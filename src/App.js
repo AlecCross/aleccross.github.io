446245a2f4bfa4db64cce8c703572b8a90224871
AlecCross <cross2472@gmail.com>
2021-10-28 05:37:18 +0300

    Добавил данные о проектах на Unity

--- src/App.js
+++ src/App.js
@@ -1,5 +1,24 @@
+import projectList from "./data/projectList";
+
 function App() {
-  return <h1 style={{textAlign: "center"}}>alec_cross.github.io</h1>
+    return <div style={{textAlign: "center"}}>
+        <h1 style={{textAlign: "center"}}>alec_cross.github.io</h1>
+        {projectList.map(
+            project => <>
+                <hr/>
+                <a style={{textDecoration: "none", color: "black"}}
+                   href={project.link}
+                   target="_blank"
+                   rel="noopener noreferrer"
+                >
+                    <img src={project.image} alt={project.title}/>
+                    <h2>{project.title}</h2>
+                    <h3>{project.description}</h3>
+                </a>
+            </>
+        )}
+    </div>
+
 }
 
 export default App
